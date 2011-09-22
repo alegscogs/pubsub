d31772cdce67e7355a49d989a5f22d30b4dbf0a9
Alex Cox <alexc@patch.com>
2011-09-22 14:40:49 -0400

    And again

--- pubsub.js
+++ pubsub.js
@@ -1,7 +1,10 @@
-// Copyright Alex Cox, but anyone can do whatever they want with this without caring about me.  
-// Anyone can even tell someone else that they have to pay a lot for it, because I can only imagine that happening in The Enterprise, and people there pay for worse things, like Omniture.
+// Copyright Alex Cox, but anyone can do whatever they want with this without 
+// caring about me.  
+// Anyone can even tell someone else that they have to pay a lot for it, 
+// because pay for worse things, like Omniture.
 //
-// Based on Peter Higgins (https://github.com/phiggins42/bloody-jquery-plugins) implementation, with changes to the style of arguments.
+// Based on Peter Higgins (https://github.com/phiggins42/bloody-jquery-plugins) 
+// implementation, with changes to the style of arguments.
 
 (function($){
 
